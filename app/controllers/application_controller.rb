2f33dec53eae88b32334bd6dd3e17c9eedaf79aa
doremi <yuuri.418@gmail.com>
2026-02-22 23:23:27 +0900

    ログイン機能の修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  before_action :basic_auth
+  before_action :basic_auth, unless: :devise_controller?
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   private
