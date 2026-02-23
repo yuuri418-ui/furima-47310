905830788c1a37f3cf3c05ddec123130c95668f2
doremi <yuuri.418@gmail.com>
2026-02-23 17:36:36 +0900

    テストの準備（FactoryBot）

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,17 @@
 FactoryBot.define do
   factory :item do
-    
+    name { '商品名' }
+    info { '商品の説明' }
+    category_id { 2 }
+    sales_status_id { 2 }
+    shipping_fee_status_id { 2 }
+    prefecture_id { 2 }
+    scheduled_delivery_id { 2 }
+    price { 500 }
+    association :user
+
+    after(:build) do |item|
+      item.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png', content_type: 'image/png')
+    end
   end
 end
