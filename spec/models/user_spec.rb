5c1c72f4988f6e7657abc1ede1a0d6d7ee535f2c
doremi <yuuri.418@gmail.com>
2026-02-23 14:54:41 +0900

    テストコードの修正
    password_confirmationのバリデーションテストコードを修正

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -30,7 +30,7 @@ RSpec.describe User, type: :model do
       it 'password_confirmationが空では登録できない' do
         @user.password_confirmation = ''
         @user.valid?
-        expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
+        expect(@user.errors.full_messages).to include("Password confirmation can't be blank")
       end
       it 'passwordとpassword_confirmationが異なる場合は登録できない' do
         @user.password = '123abc'
