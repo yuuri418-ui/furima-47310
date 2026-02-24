258f4cdb970ed5d7c8786377260e68c7f5a090de
doremi <yuuri.418@gmail.com>
2026-02-24 14:24:48 +0900

    手数料や利益の計算の表示

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -1,18 +1,24 @@
 const price = () => {
   const priceInput = document.getElementById("item-price");
-  priceInput.addEventListener("input", () => {
-    const inputValue = priceInput.value;
-    const addTaxDom = document.getElementById("add-tax-price");
-    const profitDom = document.getElementById("profit");
+  if (priceInput) {
+    const calculateAndDisplay = () => {
+      const inputValue = priceInput.value;
+      const addTaxDom = document.getElementById("add-tax-price");
+      const profitDom = document.getElementById("profit");
 
-    // 手数料（10%）の計算。Math.floorで小数点以下を切り捨てます。
-    const tax = Math.floor(inputValue * 0.1);
-    addTaxDom.innerHTML = tax;
+      const tax = Math.floor(inputValue * 0.1);
+      addTaxDom.innerHTML = tax;
 
-    // 利益の計算
-    const profit = inputValue - tax;
-    profitDom.innerHTML = profit;
-  })
+      const profit = inputValue - tax;
+      profitDom.innerHTML = profit;
+    };
+
+    priceInput.addEventListener("input", calculateAndDisplay);
+
+    if (priceInput.value) {
+    calculateAndDisplay();
+    }
+  }
 };
 
 // ページが読み込まれたとき、およびTurboによる遷移時に発火させる
